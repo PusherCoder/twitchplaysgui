5169e68d542c6d49fd0b259062af88ecd825958f
PusherCoder <bendegn@gmail.com>
2023-02-08 21:01:42 -0800

    Attempting to increase robustness of how Python is defined in the system.

--- first_time_setup.bat
+++ first_time_setup.bat
@@ -1,3 +1,4 @@
-python -m venv ./venv
+WHERE python >nul
+IF %ERRORLEVEL% NEQ 0 python -m venv ./venv ELSE py -m venv ./venv
 .\venv\Scripts\python.exe -m pip install -r requirements.txt
 .\venv\Scripts\python.exe twitchplaysgui.py
